9dc5111cce447a21fa3f665e89ef65188dc32a18
raghvendrasingh1005 <raghvendrasingh21@gmail.com>
2023-05-04 02:38:14 +0530

    added new objects

---
+++ migrations/EXECUTION.sql
@@ -0,0 +1,10 @@
+!source migrations/V1.1.1__initial_objects.sql;
+!source migrations/V1.1.2__updated_objects.sql;
+
+!source migrations/Tables/PARTY.sql;
+!source migrations/Tables/MAPPING.sql;
+!source migrations/Tables/ACCOUNT.sql;
+
+!source migrations/Views/vwPARTY.sql;
+
+
--- migrations/test.sql
+++
@@ -1,2 +0,0 @@
-!source migrations/V1.1.1__initial_objects.sql;
-!source migrations/V1.1.2__updated_objects.sql;
