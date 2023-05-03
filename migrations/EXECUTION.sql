347abd86c4048521adbf825528cee9748fec7385
raghvendrasingh1005 <raghvendrasingh21@gmail.com>
2023-05-04 04:31:19 +0530

    removed space

--- migrations/EXECUTION.sql
+++ migrations/EXECUTION.sql
@@ -1,11 +1,9 @@
 !source migrations/V1.1.1__initial_objects.sql;
 !source migrations/V1.1.2__updated_objects.sql;
-
 !source migrations/Tables/PARTY.sql;
 !source migrations/Tables/MAPPING.sql;
 !source migrations/Tables/ACCOUNT.sql;
 !source migrations/Tables/PARTY1.sql;
-
 !source migrations/Views/vwPARTY.sql;
 
 
