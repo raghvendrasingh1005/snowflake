7c5f3878d8de4f4733de131a2ab03b5f0882f273
raghvendrasingh1005 <raghvendrasingh21@gmail.com>
2023-05-05 02:46:36 +0530

    create new table

--- migrations/EXECUTION.sql
+++ migrations/EXECUTION.sql
@@ -1,4 +1,4 @@
-!source migrations/Tables/PARTY.sql;
+!source .\Tables\PARTY.sql;
 --!source migrations/Tables/MAPPING.sql;
 --!source migrations/Tables/ACCOUNT.sql;
 --!source migrations/Views/vwPARTY.sql;
