67e2bfd12c1c6c1d1268aab402e6f3dcc2493fc9
raghvendrasingh1005 <raghvendrasingh21@gmail.com>
2023-05-05 16:40:02 +0530

    sp added

--- migrations/EXECUTION.sql
+++ migrations/EXECUTION.sql
@@ -1,10 +1,11 @@
-!source migrations/Tables/PARTY1.sql
+--!source migrations/Tables/PARTY1.sql
 --!source migrations/Tables/MAPPING.sql;
 --!source migrations/Tables/ACCOUNT.sql;
 --!source migrations/Views/vwPARTY1.sql;
 --!source migrations/StoredProcedures/SP_PARTY.sql
 --!source migrations/Functions/UDF_PARTY.sql
-!source migrations/Post-Scripts/PARTY-INSERT.SQL
-!source migrations/Tables/CUSTOMER.sql
-
+--!source migrations/Post-Scripts/PARTY-INSERT.SQL
+--!source migrations/Tables/CUSTOMER.sql
+!source migrations/Views/vwCUSTOMER.sql;
+!source migrations/StoredProcedures/SP_CUSTOMER.sql;
 
