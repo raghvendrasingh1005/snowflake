7447c25ccba52e20410a566ad965cb270746426e
raghvendrasingh1005 <raghvendrasingh21@gmail.com>
2023-05-05 15:53:02 +0530

    alter table

--- migrations/EXECUTION.sql
+++ migrations/EXECUTION.sql
@@ -1,9 +1,9 @@
 !source migrations/Tables/PARTY1.sql
-!source migrations/Tables/MAPPING.sql;
-!source migrations/Tables/ACCOUNT.sql;
-!source migrations/Views/vwPARTY1.sql;
-!source migrations/StoredProcedures/SP_PARTY.sql
-!source migrations/Functions/UDF_PARTY.sql
-!source migrations/Post-Scripts/PARTY-INSERT.SQL
+--!source migrations/Tables/MAPPING.sql;
+--!source migrations/Tables/ACCOUNT.sql;
+--!source migrations/Views/vwPARTY1.sql;
+--!source migrations/StoredProcedures/SP_PARTY.sql
+--!source migrations/Functions/UDF_PARTY.sql
+--!source migrations/Post-Scripts/PARTY-INSERT.SQL
 
 
