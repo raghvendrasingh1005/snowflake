95a24d8e1708b472ffa4b42592a5c61c2848f55b
raghvendrasingh1005 <raghvendrasingh21@gmail.com>
2023-05-08 00:42:56 +0530

    creating user atble

--- migrations/EXECUTION.sql
+++ migrations/EXECUTION.sql
@@ -1,13 +1,13 @@
-BEGIN TRANSACTION;
 --!source migrations/Tables/PARTY1.sql
 --!source migrations/Tables/MAPPING.sql;
 --!source migrations/Tables/ACCOUNT.sql;
 --!source migrations/Views/vwPARTY1.sql;
 --!source migrations/StoredProcedures/SP_PARTY.sql
 --!source migrations/Functions/UDF_PARTY.sql
-!source migrations/Post-Scripts/PARTY-INSERT.SQL
+--!source migrations/Post-Scripts/PARTY-INSERT.SQL
 --!source migrations/Tables/CUSTOMER.sql
 --!source migrations/Views/vwCUSTOMER.sql;
 --!source migrations/StoredProcedures/SP_CUSTOMER.sql;
-COMMIT;
+!source migrations/Tables/USER.sql
+
 
