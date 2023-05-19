1bb6315448af3f96661e7ac450c22cf7122bd495
raghvendrasingh1005 <44678489+raghvendrasingh1005@users.noreply.github.com>
2023-05-19 22:18:50 +0530

    Update R__fn_area_of_circle.sql

--- migrations/Functions/R__fn_area_of_circle.sql
+++ migrations/Functions/R__fn_area_of_circle.sql
@@ -1,9 +1,11 @@
 USE SCHEMA POC;
 
-CREATE FUNCTION IF NOT EXISTS area_of_circle(radius FLOAT)
-  RETURNS FLOAT
-  AS
-  BEGIN
-    pi() * radius * radius
-  END
-  ;
+CREATE FUNCTION IF NOT EXISTS area_of_circle()
+    RETURNS TABLE(msg VARCHAR)
+    AS
+    $$
+        SELECT 'Hello'
+        UNION
+        SELECT 'World'
+    $$;
+  
