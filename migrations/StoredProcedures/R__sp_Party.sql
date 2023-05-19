7817aee752b4237733eec9be93980f0ccfde83c0
raghvendrasingh1005 <44678489+raghvendrasingh1005@users.noreply.github.com>
2023-05-19 22:46:51 +0530

    Update R__sp_Party.sql

--- migrations/StoredProcedures/R__sp_Party.sql
+++ migrations/StoredProcedures/R__sp_Party.sql
@@ -1,8 +1,14 @@
 USE SCHEMA POC;
 
-CREATE OR REPLACE PROCEDURE SP_PARTY(message VARCHAR)
-RETURNS VARCHAR NOT NULL
-AS
+create or replace procedure sp_Party(from_table string, to_table string, count int)
+  returns string
+  language python
+  runtime_version = '3.8'
+  packages = ('snowflake-snowpark-python')
+  handler = 'run'
+as
 $$
-  RETURN message;
+def run(session, from_table, to_table, count):
+  session.table(from_table).limit(count).write.save_as_table(to_table)
+  return "SUCCESS"
 $$;
