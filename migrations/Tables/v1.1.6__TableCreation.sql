21f706c90003626fe23d97b95870139d863a889a
raghvendrasingh1005 <raghvendrasingh21@gmail.com>
2023-05-26 17:32:09 +0530

    create table in multiple scheam

---
+++ migrations/Tables/v1.1.6__TableCreation.sql
@@ -0,0 +1,27 @@
+USE SCHEMA TEST;
+
+create table TEST_CICD_1 (
+    User_code char(2),
+    User_name varchar);
+
+USE SCHEMA POC;
+
+create table MetaData_1 (
+    User_code char(2),
+    User_name varchar);
+
+USE SCHEMA TEST;
+
+create table TEST_CICD_2 (
+    User_code char(2),
+    User_name varchar);
+
+create table TEST_CICD_3 (
+    User_code char(2),
+    User_name varchar);
+
+USE SCHEMA POC;
+
+create table MetaData_2 (
+    User_code char(2),
+    User_name varchar);
